bdc1508544ae223cb341bd402a4e92a7262b3ada
jw <wjd10577@naver.com>
2026-03-22 11:34:09 +0900

    feat: find object by embeddings

--- app/api/query.py
+++ app/api/query.py
@@ -4,13 +4,19 @@ from sqlalchemy.orm import Session
 
 from app.core.config import OPENAI_API_KEY
 from app.db.database import get_db
+from app.db.models import CodeChunk
 
 router = APIRouter(prefix="/query")
 
 client = OpenAI(api_key=OPENAI_API_KEY)
 
 @router.post("")
-def query(question: str, db: Session = Depends(get_db)):
+def query(queryRequest: QueryRequest, db: Session = Depends(get_db)):
+    # insert_test_chunk(db)
+
+    repo_id = queryRequest.repo_id
+    question = queryRequest.question
+
     query_embeddings = client.embeddings.create(
         model="text-embedding-3-small",
         input=question
@@ -18,13 +24,33 @@ def query(question: str, db: Session = Depends(get_db)):
 
     query_vector = query_embeddings.data[0].embedding
 
-    print(len(query_vector))
+    results = (
+        db.query(CodeChunk)
+            .where(CodeChunk.repo_id == repo_id)
+            .order_by(CodeChunk.embedding.l2_distance(query_vector))
+            .limit(3)
+            .all()
+    )
 
-    context = """
-    function createUser(user) {
-        return db.save(user)
-    }
-    """
+    references = []
+    context_parts = []
+
+    for chunk in results:
+        references.append({
+            "file_path": chunk.file_path,
+            "function_name": chunk.function_name,
+            "start_line": chunk.start_line,
+            "end_line": chunk.end_line,
+        })
+
+        context_parts.append(f"""
+            파일: {chunk.file_path}
+            함수: {chunk.function_name}
+
+            {chunk.content}
+            """)
+
+    context = "\n".join(context_parts)
 
     prompt = f"""
     다음 코드를 참고해서 질문에 답해라.
@@ -44,5 +70,37 @@ def query(question: str, db: Session = Depends(get_db)):
     )
 
     return {
-        "answer": response.choices[0].message.content
+        "answer": response.choices[0].message.content,
+        "references": references
     }
+
+class QueryRequest:
+    repo_id: int
+    question: str
+
+def insert_test_chunk(db: Session):
+    code = """
+    def create_user(user):
+        return db.save(user)
+    """
+
+    embeddings_response = client.embeddings.create(
+        model="text-embedding-3-small",
+        input=code
+    )
+
+    vector = embeddings_response.data[0].embedding
+
+    chunk = CodeChunk(
+        repo_id=1,
+        content=code,
+        embedding=vector,
+        file_path="user_service.py",
+        function_name="create_user",
+        language="python",
+        start_line=1,
+        end_line=3
+    )
+
+    db.add(chunk)
+    db.commit()
